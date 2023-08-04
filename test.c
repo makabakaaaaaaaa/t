2c57be3d9c9103f2f5de2ae5819d28b87a353cd3
makabakaaaaaaaa <139683009+makabakaaaaaaaa@users.noreply.github.com>
2023-08-04 10:55:15 +0800

    Add files via upload

--- test.c
+++ test.c
@@ -1,79 +1,14 @@
-#define _CRT_SECURE_NO_WARNINGS	
-#include<string.h>
-#include<stdio.h>
-#include"game.h"
-void game()
-{
-	char ret = 0;
-	char board[ROW][COL] = { 0 };
-	initboard(board, ROW, COL);
-	displayboard(board,ROW,COL);
-	while (1)
-	{
-		playerturn(board,ROW,COL);
-		displayboard(board,ROW,COL);
-		ret=is_win(board,ROW,COL);
-		if (ret == '*')
-		{
-			printf("Congrantulations!You win!\n");
-		}
-		if (ret == 'q')
-		{
-			printf("DRAW！\n");
-		}
-		if (ret != 'c')
-		{
-			break;
-		}
-		computerturn(board,ROW,COL);
-		displayboard(board,ROW,COL);
-		ret=is_win(board,ROW,COL);
-		if (ret == '#')
-		{
-			printf("I am sorry that you fail\n");
-		}
-		if (ret == 'q')
-		{
-			printf("DRAW!\n");
-		}
-		if (ret != 'c')
-		{
-			break;
-		}
-		
-	}
-}
-void menu()
-{
-	printf("*******************\n");
-	printf("***1.play 0.exit***\n");
-	printf("*******************\n");
-	printf("choose number:");
-}
-void test()
-{
-	int input = 0;
-	srand((unsigned int)time(NULL));
-	do
-	{
-		menu();
-		scanf_s("%d", &input);
-		switch (input)
-		{
-		case 1:
-			game();
-			break;
-		case 0:
-			printf("退出游戏\n");
-			break;
-		default:
-			printf("错误，请重新输入：\n");
-			break;
-		}
-	} while (input);
-}
+#include <stdio.h>
 main()
 {
-	test();
+	int water_num = 0;
+	scanf_s("%d", &water_num);
+	int sum = water_num/2;
+
+	while (water_num>=2)
+	{
+		sum = sum + water_num / 2;
+	}
+	printf("%d", num);
 	return 0;
 }
